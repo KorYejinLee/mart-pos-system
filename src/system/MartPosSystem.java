8b0b3d0ecde83c56abf296610ff70f2ce64a4fe8
Yejin Lee <77622755+KorYejinLee@users.noreply.github.com>
2025-02-03 21:34:54 +0900

    정적 팩토리 메서드 패턴을 적용 (#3)
    * # title
    - 배열로 생성한 객체를 객체라고 할 수 없다
    
    # what is different
    - 초기값 세팅하는 네 가지 배열 삭제
    - Products 생성자를 private화
    - 정적 팩토리 메소드를 적용하여 Products 초기화
    - Products 배열 객체로 List 타입 객체 생성
    
    # description
    - 좀 더 가독성 좋은 코드를 작성하고 객체 지향적으로 프로그래밍 하기 위해서 정적 팩토리 메서드 패턴을 사용
    - List의 of(), List 인터페이스의 of() 메서드는 인터페이스를 반환하는 정적 팩토리 메서드이다.
    
    * # title
    - Publics 클래스 상속을 이용할 수 없다.
    
    # what is different
    - 초기 세팅이 실행될 수 있도록 Public 클래스 상속 삭제
    - getProducts() static method 사용
    
    # description
    - 정적 팩토리 메서드를 제공하면 private 생성자를 활용하게 되므로 상속을 이용할 수 없다.
    - 이름 짓기 강의를 먼저 완료한 후에, 리팩토링하고자 한다.
    - 현재 초기 세팅만 동작되도록 나머지 기능은 주석처리 하였다.

--- src/system/MartPosSystem.java
+++ src/system/MartPosSystem.java
@@ -1,108 +1,106 @@
 package system;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Scanner;
 
 import db.Products;
 import db.User;
-import logic.ExpirationDate;
 import logic.Login;
-import logic.Sale;
 import view.SystemInput;
 import view.SystemView;
 
-import static db.Products.setInitProductsSize;
+import static db.Products.*;
 
 public class MartPosSystem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		setInitProductsSize(10);
+		List<Products> products = generateInitProducts();
 
-		Products products = new Products(null, 0, 0, false, null);
+//		Products products = new Products(null, 0, 0, false, null);
 		User user = new User();
 		Login login = new Login();
 		LocalDateTime currentTime;
-		Sale sale = new Sale(null, 0, 0, false, null);
-		ExpirationDate ed = new ExpirationDate(null, 0, 0, false, null);
+//		Sale sale = new Sale(null, 0, 0, false, null);
+//		ExpirationDate ed = new ExpirationDate(null, 0, 0, false, null);
 		Scanner scanner = new Scanner(System.in);
 		boolean logined = login.isLogined();
-        boolean worked = true;
+		boolean worked = true;
 		boolean mainBack = false;
-
-		products.generateInitProduct(); // 초기 물품 데이터 넣기
 
 		SystemView.startView();
 		SystemView.loginView();
 		logined = login.isLogined();
-	
-        while(!logined) {
-        	System.out.println("══════════════════════════════════════════");
-		    System.out.println("              ID를 입력해주세요.");
-		    System.out.println("══════════════════════════════════════════");
-		    String inputId = scanner.nextLine().trim();
-		    System.out.println("══════════════════════════════════════════");
-		    System.out.println("           PASSWORD를 입력해주세요.");
-		    System.out.println("══════════════════════════════════════════");
-		    String inputPW = scanner.nextLine().trim();
+
+		while(!logined) {
+			System.out.println("══════════════════════════════════════════");
+			System.out.println("              ID를 입력해주세요.");
+			System.out.println("══════════════════════════════════════════");
+			String inputId = scanner.nextLine().trim();
+			System.out.println("══════════════════════════════════════════");
+			System.out.println("           PASSWORD를 입력해주세요.");
+			System.out.println("══════════════════════════════════════════");
+			String inputPW = scanner.nextLine().trim();
 			login.checkUserInfo(inputId, inputPW); // 유저 정보 체크
 			logined = login.isLogined();
 
-	        if (!logined) {
-	            System.out.println("══════════════════════════════════════════");
-	            System.out.printf("%16s로그인 실패%-16s\n","","");
-	            System.out.println("══════════════════════════════════════════");
-	        } 
-        }
+			if (!logined) {
+				System.out.println("══════════════════════════════════════════");
+				System.out.printf("%16s로그인 실패%-16s\n","","");
+				System.out.println("══════════════════════════════════════════");
+			}
+		}
 
 		while (worked) {
-        	currentTime = LocalDateTime.now();
+			currentTime = LocalDateTime.now();
 			user.setLoginTime(currentTime);
-            SystemView.mainMenuView();
+			SystemView.mainMenuView();
 			int inputValue = SystemInput.inputNumberBetweenBy(1, 6);
-            if (inputValue == 1) {
-				products.showProduct();
+			if (inputValue == 1) {
+				showProducts(products);
 			} else if (inputValue == 2) {
 				SystemView.balanceView();
-				sale.showBalance();
+//				showBalance();
 			} else if (inputValue == 3) {
-				sale.showSale();
+//				showSale();
 			} else if (inputValue == 4) {
-				ed.showRemainingTime();
+//				showRemainingTime();
 			} else if (inputValue == 5) {
 				mainBack = false;
 				while (!mainBack) {
 					SystemView.subMenuView();
 					inputValue = SystemInput.inputNumberBetweenBy(1, 5);
 					switch (inputValue) {
-	                case 1:
-	                	sale.saleMenu();
-	                    break;
-	                case 2:
-	                	sale.refundCard();
-	                    break;
-	                case 3:
-	                    break;
-	                case 4: 
-	                	products.searchProductByName();
-	                case 5:
-	                    System.out.println("══════════════════════════════════════════");
-	                    System.out.println("              메인 메뉴로 돌아갑니다.");
-	                    System.out.println("══════════════════════════════════════════");
-	                    mainBack = true;
-	                    break;
-	                default:
-	                    System.out.println("══════════════════════════════════════════");
-	                    System.out.println("           올바른 메뉴 번호를 입력해주세요.");
-	                    System.out.println("══════════════════════════════════════════");
-	                    break;
+						case 1:
+//							saleMenu();
+							break;
+						case 2:
+//							refundCard();
+							break;
+						case 3:
+							break;
+						case 4:
+							searchProductByName();
+						case 5:
+							System.out.println("══════════════════════════════════════════");
+							System.out.println("              메인 메뉴로 돌아갑니다.");
+							System.out.println("══════════════════════════════════════════");
+							mainBack = true;
+							break;
+						default:
+							System.out.println("══════════════════════════════════════════");
+							System.out.println("           올바른 메뉴 번호를 입력해주세요.");
+							System.out.println("══════════════════════════════════════════");
+							break;
 					}
 				}
 			} else if (inputValue == 6) {
 				currentTime = LocalDateTime.now();
-				user.setLogOutTime(currentTime); 
-				
-		        int minutes1 = user.getLoginTime().getMinute();
+				user.setLogOutTime(currentTime);
+
+				int minutes1 = user.getLoginTime().getMinute();
 				int minutes2 = user.getLogOutTime().getMinute();
 				int dailyWagePerMinute = 9_800;
 				int workTime = minutes2 - minutes1;
@@ -111,12 +109,12 @@ public class MartPosSystem {
 				System.out.println("══════════════════════════════════════════");
 				System.out.printf("               일한 시간 : %d 분\n",workTime);
 				System.out.println("══════════════════════════════════════════");
-			    System.out.println("══════════════════════════════════════════");
+				System.out.println("══════════════════════════════════════════");
 				System.out.printf("               일당 : %s (원)\n",user.getDailyWage());
-			    System.out.println("══════════════════════════════════════════");
-				products.printProductStar();
+				System.out.println("══════════════════════════════════════════");
+				printProductStar();
 				worked = false;
 			}
-        }
+		}
 	}
 }
